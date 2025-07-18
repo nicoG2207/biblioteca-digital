02401c0e3bd7177b7d0f95c69decbfef15ec5a52
JarithoX <jarodpintojarol2@gmail.com>
2025-07-18 15:56:07 -0400

    Componentes (autores, prestamos)

---
+++ src/app/pages/autores/autores.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autores',
+  imports: [],
+  templateUrl: './autores.html',
+  styleUrl: './autores.scss'
+})
+export class Autores {
+
+}
---
+++ src/app/pages/prestamos/prestamos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestamos',
+  imports: [],
+  templateUrl: './prestamos.html',
+  styleUrl: './prestamos.scss'
+})
+export class Prestamos {
+
+}
